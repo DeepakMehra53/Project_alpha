32f6e3ba4bd0abdde310d8cd160e74d1999f7c0f
DeepakMehra53 <dmehra594@gmail.com>
2024-12-17 22:46:56 +0530

    "Hello bitch"

---
+++ src/components/Locomotive.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Locomotive() {
+  return (
+    <div data-scroll-section="0,5" className='locomotive-scroll'></div>
+  )
+}
+
+export default Locomotive;
---
+++ src/components/Section.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Section() {
+  return (
+    <div id='lu' data-w-id="bcbd74d8-d86f-f80a-dddc-9ce2540ad919" className="_1_st_section_wrap">
+        
+    </div>
+  )
+}
+
+export default Section

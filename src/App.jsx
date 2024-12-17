a35be8c1115cd7884dec1c562a93763acea42d9b
DeepakMehra53 <dmehra594@gmail.com>
2024-12-17 23:24:33 +0530

    Again error bitch

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Locomotive from './components/Locomotive'
+function App() {
+  return (
+    <div>
+        <Locomotive/>
+    </div>
+  )
+}
+
+export default App
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import Home from '../components/Home/Home.jsx'
 function Section() {
   return (
     <div id='lu' data-w-id="bcbd74d8-d86f-f80a-dddc-9ce2540ad919" className="_1_st_section_wrap">
-        
+        <Home/>
     </div>
   )
 }

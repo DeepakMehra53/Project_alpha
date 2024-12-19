3313f6cbcbf671bfb5f2d2c86d801790a6654fa3
DeepakMehra53 <dmehra594@gmail.com>
2024-12-19 21:10:39 +0530

    Commit commit bitch

--- src/components/Locomotive.jsx
+++ src/components/Locomotive.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Section from './Section.jsx'
 function Locomotive() {
   return (
-    <div data-scroll-section="0,5" className='locomotive-scroll'>
+    <div data-scroll-section="0,5" className='overflow-hidden locomotive-scroll'>
       <Section/>
     </div>
   )

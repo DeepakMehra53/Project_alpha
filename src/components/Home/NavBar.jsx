1261d574eb7da9692f2df2f306ddc265eecbeed8
DeepakMehra53 <dmehra594@gmail.com>
2024-12-19 21:18:43 +0530

    Navbar css

--- src/components/Home/NavBar.jsx
+++ src/components/Home/NavBar.jsx
@@ -2,10 +2,19 @@ import React from 'react'
 import Container from './Container'
 function NavBar() {
   return (
-    <div data-collapse="medium" data-animation="default" data-duration="800" data-w-id="045f99fe-ac6b-eb28-1f16-d72c0e5c6055" data-easing="ease" data-easing2="ease" role="banner" className='navbar w-nav'>
-      <Container/>
+    <div
+      data-collapse="medium"
+      data-animation="default"
+      data-duration="800"
+      data-w-id="045f99fe-ac6b-eb28-1f16-d72c0e5c6055"
+      data-easing="ease"
+      data-easing2="ease"
+      role="banner"
+      className="absolute top-0 left-0 right-0 bottom-auto h-[5.8em] bg-transparent bg-#dddddd z-1000 navbar w-nav"
+    >
+      <Container />
     </div>
-  )
+  );
 }
 
 export default NavBar
